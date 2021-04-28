c7ea191f31020c6d1c11f5f361c0041665e2589d
omanshom <abhaykritsingh@gmail.com>
2021-04-28 12:43:18 +0530

    we added box8

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var box8;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    box8 = new Box(600,200);
 
 }
 
@@ -61,4 +63,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    box8.display();
 }
